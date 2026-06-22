cb59b4d2a410ec4db46c1b6894dfd091c085274f
Luberri <fandresimahery@gmail.com>
2026-06-22 09:55:48 +0300

    update 2026-06-22 09:55:47

--- eval2/e2/src/components/spring/Modif.vue
+++ eval2/e2/src/components/spring/Modif.vue
@@ -1,5 +1,13 @@
 <script setup>
+import { onMounted, ref } from 'vue';
+import { getAllCoutSpring } from '../../services/spring';
+
+const all = ref([])
+onMounted(async () => {
+    all.value = (await getAllCoutSpring)
+})
 </script>
 <template>
     <h1>reouverture et cout</h1>
+    
 </template>
